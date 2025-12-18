c58b4340b93b382b67d23b5a2e6087cb65449392
Michael Kiwanuka <mkiwa37@gmail.com>
2025-12-18 08:42:41 -0500

    Add additional function keys to keypresses

--- Input.js
+++ Input.js
@@ -99,7 +99,7 @@ Object.defineProperty(globalThis, "Input", {
         for (let i = 33; i <= 126; i++) {
             keypresses.addKey(String.fromCharCode(i))
         }
-        ["shift", "control", "metakey", "alt", "tab", "enter", "backspace", "escape", "space", "arrowup", "arrowdown", "arrowleft", "arrowright", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14"]
+        ["shift", "control", "metakey", "alt", "tab", "enter", "backspace", "escape", "space", "arrowup", "arrowdown", "arrowleft", "arrowright", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15", "F16", "F17", "F18", "F19", "F20", "F21", "F22", "F23", "F24"]
         .forEach(function (e) {
             Object.defineProperty(keypresses, e, 
             {  value: new Key(), 
